1269e6cf965a1e52eea5b0ab24bd84a633d44176
Aryaun <ariankhamesi@gmail.com>
2024-04-22 20:07:54 +0330

    coding signup api

---
+++ src/app/api/auth/signup/route.js
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/utils/connectDB";
+import { hashPassword } from "@/utils/auth";
+import User from "src/models/Users";
+
+export async function POST(req) {
+    try {
+        await connectDB()
+
+        const { userName, email, password } = await req.json()
+        console.log(userName, email, password);
+
+
+        if (!userName || !email || !password) {
+            return NextResponse.json(
+                { error: "plz enter correct data and complete fillds" },
+                { status: 422 }
+            )
+        }
+
+
+        const existingUser = await User.findOne({ email })
+        console.log(existingUser);
+
+        if (existingUser) {
+            return NextResponse.json(
+                { error: "user exist" },
+                { status: 422 }
+            )
+        }
+
+
+        const hashedPassword = hashPassword(password)
+
+        const newUser = await User.create({
+            userName,
+            email,
+            password: hashedPassword
+        })
+        console.log(newUser);
+
+        return NextResponse.json(
+            { message: "User successfully created" },
+            { status: 200 }
+        )
+
+    }
+    catch (err) {
+        console.log(err);
+        return NextResponse.json(
+            { error: "some things wrong in server" },
+            { status: 500 }
+        )
+    }
+}
