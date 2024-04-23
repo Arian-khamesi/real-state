db1ad3ac40db9d281305a6fef9ccc4e92eba8510
Aryaun <ariankhamesi@gmail.com>
2024-04-23 17:45:12 +0330

    coding sidebar and  handle authorize

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+function Dashboard(){
+    return(
+        <div>hhi</div>
+    )
+}
+
+
+export default Dashboard
---
+++ src/components/layout/DashboardSidebar.js
@@ -0,0 +1,26 @@
+import { getSession } from "next-auth/react";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import styles from "../layout/DashboardSidebar.module.css"
+import { CgProfile } from "react-icons/cg";
+import Link from "next/link";
+
+async function DashboardSidebar({ children }) {
+
+    const session = await getSession(authOptions)
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                <CgProfile />
+                <p>{session?session.user.email:null}</p>
+                <span></span>
+                <Link href="/dashboard">حساب کاربری</Link>
+                <Link href="/dashboard/my-profiles">آگهی های من</Link>
+                <Link href="/dashboard/add">ثبت آگهی</Link>
+            </div>
+            <div className={styles.main}>{children}</div>
+        </div>
+    )
+}
+
+export default DashboardSidebar
