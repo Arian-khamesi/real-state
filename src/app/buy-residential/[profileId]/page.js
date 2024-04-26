a3975237b90a30f1b5e2d4a2fe18ae80a148c56b
Aryaun <ariankhamesi@gmail.com>
2024-04-26 22:44:46 +0330

    add main profiles page

---
+++ src/app/buy-residential/[profileId]/page.js
@@ -0,0 +1,27 @@
+
+import DetailsPage from "@/template//DetailsPage";
+import connectDB from "@/utils/connectDB";
+import Profile from "src/models/Profile";
+
+async function ProfileDetails({ params: { profileId } }) {
+  await connectDB();
+  const profile = await Profile.findOne({ _id: profileId });
+
+  if (!profile) return <h3>مشکلی پیش آمده است</h3>;
+
+  return <DetailsPage data={profile} />;
+}
+
+export default ProfileDetails;
+
+export const generateMetadata = async ({ params: { profileId } }) => {
+  await connectDB();
+  const profile = await Profile.findOne({ _id: profileId });
+
+  return {
+    title: profile.title,
+    description: profile.description,
+    authors: { name: profile.realState },
+    other: { mytag: "test meta tag" },
+  };
+};
